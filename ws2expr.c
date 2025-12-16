1dd8a872ec6a421311a75766e00373b68b1f11ef
6804062612056 <s6804062612056@email.kmutnb.ac.th>
2025-12-16 09:16:07 +0700

    Add files via upload

---
+++ ws2expr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	float a,b,c,d;
+	scanf("%f %f %f %f",&a,&b,&c,&d);
+	printf("%f\n",3*a/5+1/b);
+	printf("%f\n",(3*a+5*b)/(2+c));1;
+	printf("%f\n",2.0/7*(pow(4,3+c)-5*d));
+	printf("%f\n",sqrt((2+8*b)/a));
+	printf("%f\n",cbrt(pow(b,2)-4*d));
+}
